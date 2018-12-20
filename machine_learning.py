329330a338a4c999ad63624d96b242683e9c62f9
marasluna <marasluna@gmail.com>
2018-12-20 11:10:36 -0800

    update payloads and clean predict route

--- machine_learning.py
+++ machine_learning.py
@@ -12,27 +12,45 @@ BUCKET_NAME = ''
 MODEL_FILE_NAME = 'logistic_regression.pkl'
 MODEL_LOCAL_PATH = '/Users/marasluna/projects/machine-learning-mnist/model'
 
+NUM_COEF = 784
+
 
 @app.route('/')
 def index():
     return "A machine learning engine for classifying MNIST dataset."
 
 
-@app.route('/predict/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     # get payload
     payload = json.loads(request.get_data())
+    # transform payload to panda data
+    data = json_to_data(payload)
     # predict
-    prediction = predict_model(payload["payload"])
-    # get response
-    data = {}
-    data['data'] = prediction
-    return json.dumps(data)
+    prediction = predict_model(data)
+    # transform prediction to json
+    response = prediction_to_json(data.index, prediction)
+    return json.dumps(response)
 
 
-@app.route('/test/', methods=['POST'])
+@app.route('/test', methods=['POST'])
 def test():
     return 'Test'
+
+
+def json_to_data(payload):
+    panda_data = pd.DataFrame.from_dict(
+        payload["payload"]["data"], orient="index")
+    panda_data.columns = pd.to_numeric(panda_data.columns)
+    panda_data.sort_index(axis=1, inplace=True)
+
+    return panda_data
+
+
+def prediction_to_json(index, prediction):
+    df = pd.DataFrame(index=index,
+                      data=prediction, columns=["prediction"])
+    return json.loads(df.to_json())
 
 
 def load_model():
@@ -47,10 +65,5 @@ def load_model():
 
 
 def predict_model(data):
-	panda_data = pd.DataFrame.from_records(data)
-	panda_data.columns = pd.to_numeric(panda_data.columns)
-	panda_data.sort_index(axis=1, inplace=True)
-
-	clf = load_model()
-	return clf.predict(panda_data).tolist()
-
+    clf = load_model()
+    return clf.predict(data).tolist()
