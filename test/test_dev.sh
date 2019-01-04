074c9eae3e4504ea6ae56576ec7b88f5be26db8c
marasluna <marasluna@gmail.com>
2019-01-04 15:05:33 -0800

    add dev test on AWS lambda

---
+++ test/test_dev.sh
@@ -0,0 +1,2 @@
+curl -X POST --data @data/sample_data.json https://xxx.execute-api.us-west-1.amazonaws.com/dev/predict
+curl -X POST --data @data/sample.json https://xxx.execute-api.us-west-1.amazonaws.com/dev/score
